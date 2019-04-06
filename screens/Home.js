60fcd6821527ba5c589a53f0da9c696aa9d7c502
Robert Logan <bigdog_41@hotmail.com>
2019-04-06 15:26:28 -0700

    add login button

--- screens/Home.js
+++ screens/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 import { Button } from "native-base";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 var backgroundImage = require("../assets/city_traffic3.jpg");
 
 class Home extends Component {
@@ -25,33 +26,54 @@ class Home extends Component {
             style={{ flex: 1, width: null, height: null }}
           />
         </View>
+        <TouchableOpacity style={styles.fabBtn}>
+          <MaterialCommunityIcons name="toilet" size={70} color="black" />
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.fabBtn}
+          onPress={() => navigate("Explore")}
+        >
+          <MaterialCommunityIcons name="food" size={70} color="black" />
+        </TouchableOpacity>
+        {/* <TouchableOpacity>
+          <Text style={styles.btn}>Login/SignUp</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btn}>
+          <Text style={styles.btn}>Add Your Restaurant Here</Text>
+        </TouchableOpacity> */}
+
         <View>
           <Button
-            rounded
+            bordered
+            info
             style={{
-              paddingLeft: 40,
-              paddingRight: 40,
-              marginBottom: 40,
-              backgroundColor: "cyan"
+              backgroundColor: "#FFF",
+              marginBottom: 20,
+              justifyContent: "center",
+              alignItems: "center"
+              //   paddingLeft: 40,
+              //   paddingRight: 40,
+              //   marginBottom: 40,
+              //   backgroundColor: "cyan"
             }}
           >
-            <Text style={{ fontSize: 24, color: "#fff", fontWeight: "bold" }}>
-              Find A Bathroom
-            </Text>
+            <Text style={{ fontSize: 18, color: "#00BFFF" }}>Login/SignUp</Text>
           </Button>
           <Button
-            rounded
+            bordered
+            info
             style={{
-              paddingLeft: 40,
-              paddingRight: 75,
-
-              backgroundColor: "#1E90FF"
+              //   paddingLeft: 40,
+              //   paddingRight: 75,
+              backgroundColor: "#FFF",
+              marginBottom: 20,
+              justifyContent: "center",
+              alignItems: "center"
             }}
             onPress={() => navigate("Explore")}
           >
-            <Text style={{ fontSize: 24, color: "#fff", fontWeight: "bold" }}>
-              Are you Hungry?
-            </Text>
+            <Text>Add Your Restaurant Here</Text>
           </Button>
         </View>
       </View>
@@ -65,5 +87,30 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center"
+  },
+  fabBtn: {
+    justifyContent: "center",
+    alignItems: "center",
+    position: "relative",
+    width: 90,
+    height: 90,
+    backgroundColor: "#fff",
+    borderRadius: 50,
+    borderColor: "black",
+    borderWidth: 2,
+    marginBottom: 20
+  },
+  btn: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: 40,
+    height: 30,
+    backgroundColor: "#fff",
+    paddingLeft: 40,
+    paddingRight: 40,
+    paddingTop: 20,
+    paddingBottom: 20,
+    fontSize: 16,
+    color: "black"
   }
 });
