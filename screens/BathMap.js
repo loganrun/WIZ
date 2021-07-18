efe41d30e7cec5c69bc82eb5e5b9dfb24bf4a953
Rob <rob@Robs-MacBook-Pro.local>
2021-07-18 16:02:59 -0700

    restore android onboarding

--- screens/BathMap.js
+++ screens/BathMap.js
@@ -32,8 +32,9 @@ var genericFood = require('../assets/SEARCH-lower-card-generic-img-1.png')
 var unverified = require('../assets/mascot-01-unverified-349x161.png')
 var verified = require('../assets/mascot-01-verified-329x161.png')
 var premicon = require('../assets/pin-verified.png')
-var premicon1 = require('../assets/piniverified1.png')
+//var premicon1 = require('../assets/pin-verified2.png')
 var regIcon = require('../assets/pin-unverified.png')
+//var regIcon1 = require('../assets/pin-unverified1.png')
 //import * as Analytics from 'expo-firebase-analytics'
 import * as Amplitude from 'expo-analytics-amplitude'
 import axios from 'axios'
@@ -88,7 +89,7 @@ constructor(props) {
     
     this.loadBathroom();
     this.setState({ loading: true });
-    //this.useCheck();
+    this.useCheck();
     //this.index = 0;
     //this.animation = new Animated.Value(0);
     
@@ -98,11 +99,11 @@ constructor(props) {
 
 
 
-  // useCheck = async () =>{
-  // newUser = this.props.user
-  // this.setState({ newUser: true })
+  useCheck = async () =>{
+  newUser = this.props.user
+  this.setState({ newUser: true })
   
-  // }
+  }
   
   setMargin=()=>{
     this.setState({mapMargin: 0});
@@ -322,8 +323,8 @@ constructor(props) {
           longitude: item.longitude
         }}
         //title={item.name}
-        image={premicon1}
-        style={{height: 30, width: 30}}
+        image={premicon}
+        //style={{height: 30, width: 30}}
         onPress={() => {
           const markerProp = {
           id: item.id,
@@ -350,7 +351,7 @@ constructor(props) {
             }}
             //title={item.name}
             image={regIcon}
-            style={{height: 30, width: 30}}
+            //style={{height: 30, width: 30}}
             onPress={() => {
               const markerProp = {
               id: item.id,
